b72041aab43c227b3964986f0626624cf3de0b0d
taha1461 <114801967+taha1461@users.noreply.github.com>
2022-10-08 11:59:50 +0300

    matematiksel operatörler öğrenildi

---
+++ matematiksel operatorler.py
@@ -0,0 +1,34 @@
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+
+#toplama
+print(5487+9241)
+
+#çıkartma
+print(42-17)
+
+#çarpma
+print(12*16)
+
+#bölme
+print(81/4)
+
+#üs alma
+print(2**3)
+print(2**3**4)
+
+#mode alma
+print(40%3)
+
+#tam bölme
+print(40//3)  # 13
+print(-40//3)  # -14
+
+
