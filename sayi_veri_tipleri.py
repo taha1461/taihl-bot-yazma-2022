9b03de25c0449653860d9a33fb3453407ad96961
taha1461 <114801967+taha1461@users.noreply.github.com>
2022-10-08 11:32:25 +0300

    sayı veri tipleri öğrenildi

---
+++ sayi_veri_tipleri.py
@@ -0,0 +1,15 @@
+"""
+İki tür veri tipi vardır: integer, float (numbers)
+    int: 1, 75
+    float: 8.5, 12.8
+"""
+
+# tam sayılarla işlem yapalım
+print(8+7)
+
+# ondalık sayılarla işlem yapalım
+print(1.6*3.7)
+
+# verilerin tipine bakalım: type()
+print(type(5))
+print(type(19.7))
