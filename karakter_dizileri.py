e896b25740ca44d456d5f7a4fe1a064ae447e2cc
taha1461 <114801967+taha1461@users.noreply.github.com>
2022-10-15 11:33:32 +0300

    karakter dizileri bitti

---
+++ karakter_dizileri.py
@@ -0,0 +1,17 @@
+# kullanıcı yaş ve isim değişkenlerini oluşturalım
+yas = 14
+isim = "taha"
+
+# kullanıcı bilgilerini ekrana yazdıralım
+# -> Adınız : taha , Yaşınız : 14
+print("Adınız : " + isim + ",Yaşınız :" + str(yas))
+
+# karakter verisinin uzunluğuna bakalım
+print(len(isim))
+
+# str veri tipindeki değerin çeşitli elemanlarını yazdıralım
+# elemanlar 0'dan başlar
+print(isim[0])
+print(isim[1])
+print(isim[2])
+print(isim[3])
