25969047ffd28d24b45e18bf2d30f04eb4e0d66f
taha1461 <114801967+taha1461@users.noreply.github.com>
2022-10-22 12:01:09 +0300

    listeler konusu bitti

---
+++ listeler.py
@@ -0,0 +1,37 @@
+# Boş bir liste tanımlayalım:[]
+liste = []
+print(liste)
+print(type(liste))
+
+okul ="Sancaktepe Teknoloji Anadolu İmam Hatip Lisesi"
+kelimeler = okul.split()
+print(kelimeler)
+
+# Belirli sıradaki kelimeleri yazdıralım
+print(kelimeler[0]) # ilk kelime
+print(kelimeler[1])
+print(kelimeler[2])
+print(kelimeler[3])
+print(kelimeler[4])
+print(kelimeler[5])
+# print(kelimeler[6]) # hata verdi
+print(kelimeler[-1]) # son kelime
+
+ad = "eren mustafa özdal"
+print(ad[0]) # E
+print(ad[-7:-14:-1]) # afatsuM
+print(ad[5:12]) # Mustafa
+
+# Listeler içerisinde farklı türden veriler olabilir
+liste = [1, 12.3, "Python", True,[1,2,3]]
+print(liste[-1][-1]) # 3
+print(liste[4][-1]) # 3
+print(liste[4][2]) # 3
+
+# İki listeyi birleştirme
+liste2 =[1,2,3]
+liste3 =[4,5,6]
+liste4 = liste2 + liste3
+liste5 = liste3 + liste2
+print(liste4)
+print(liste5)
