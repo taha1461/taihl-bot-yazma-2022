3e581151f06510d55d5eb9a06616e130dd286bab
taha1461 <114801967+taha1461@users.noreply.github.com>
2022-10-22 11:25:39 +0300

    String kaçış dizileri bitti

---
+++ string_kacis_dizileri.py
@@ -0,0 +1,17 @@
+"""
+Python’ın gözünde bir başlangıç tırnağı ile bitiş tırnağı arasında yer alan
+her şey bir karakter dizisidir. Ancak bizler metin içerisinde bu tırnakları
+özel bir amaçla kullanmak isteyebiliriz. Bu tür durumlarda kaçış karakteri
+olan ters bölü işaretini (\) kullanarak hatadan kaçarız.
+"""
+
+print("Ahmet, \"Bügün sinemaya gidiyorum.\" dedi.")
+
+print('Yarın Adana\'ya gidiyorum.')
+
+print("C\\Users\\erenMustafaOzdal")
+
+# ÖZEL KARAKTERLER
+print("Eren\nMustafa\nÖzdal") # alt satır
+print("Ahmet\tMehmet\tYılmaz") # tab
+print("Eren\tMustafa\tÖzdal") # tab
